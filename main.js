f63296cde61d2ef6d559651412c267c86dd0572c
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 20:12:52 +0100

    ett exampel om charcodeat

--- main.js
+++ main.js
@@ -2,17 +2,82 @@
 
 /*
 charAt : (index)
+charCodeAt : (index)
 */
-
+/*
 var myString = "I Love JavaScript Too Much",
 
 
     myChar = myString.charAt(myString.length - 1);
 
-    myLength = myString.length,
+myLength = myString.length,
 
-console.log(myString);
+    console.log(myString);
 
 console.log(myLength);
 
 console.log(charAt);
+*/
+/*
+var myString = "I Love JavaScript Too Much",
+
+
+    myChar = myString.charAt(myString.length - 2);
+
+myLength = myString.length,
+
+    console.log(myString);
+
+console.log(myLength);
+
+console.log(myChar);
+*/
+
+/*
+var myString = "I Love JavaScript Too Much",
+
+
+    myChar = myString.charAt();
+
+
+    console.log(myString);
+
+
+console.log(myChar);
+*/
+/*
+var myString = "I Love JavaScript Too Much",
+
+
+    myChar = myString.charAt(2);
+
+
+    console.log(myString);
+
+
+console.log(myChar);
+*/
+
+/*
+var myString = "I Love JavaScript Too Much",
+
+
+    myChar = myString.charCodeAt();
+
+
+    console.log(myString);
+
+
+console.log(myChar);
+*/
+
+var myString = "I Love JavaScript Too Much",
+
+
+    myChar = myString.charCodeAt();
+
+
+    console.log(myString);
+
+
+console.log(myChar);
