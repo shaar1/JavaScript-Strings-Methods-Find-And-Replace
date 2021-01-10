d62ea461e5e6563e2f3790fd99a8c332dcd865e1
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 20:06:14 +0100

    ett exampel om charAt

---
+++ main.js
@@ -0,0 +1,18 @@
+/* globalconsole,alert, prompt*/
+
+/*
+charAt : (index)
+*/
+
+var myString = "I Love JavaScript Too Much",
+
+
+    myChar = myString.charAt(myString.length - 1);
+
+    myLength = myString.length,
+
+console.log(myString);
+
+console.log(myLength);
+
+console.log(charAt);
